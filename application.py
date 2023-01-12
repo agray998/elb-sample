923d69b4a77d81ae023e964aeaf1564e0dfcee94
agray998 <a.gray.4797@gmail.com>
2023-01-12 17:01:52 +0000

    trying to fix 502

--- application.py
+++ application.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from src import application, db
 
+with application.app_context():
+    db.drop_all()
+    db.create_all()
+
 if __name__ == '__main__':
-    with application.app_context():
-        db.drop_all()
-        db.create_all()
     application.run(debug = True, host = '0.0.0.0')
 # blah
