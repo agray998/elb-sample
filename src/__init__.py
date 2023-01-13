e0c055604c8e4ce71fab9a3026b024fd2ba4259d
agray998 <a.gray.4797@gmail.com>
2023-01-13 11:21:20 +0000

    added x-ray support

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired
@@ -7,6 +9,9 @@ import pymysql
 import os
 
 application = Flask(__name__)
+
+xray_recorder.configure(service='My application')
+XRayMiddleware(application, xray_recorder)
 
 application.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
 application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
